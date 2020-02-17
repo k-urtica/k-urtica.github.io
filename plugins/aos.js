23c6280bebc1df28aef1a8cd85c36ae333cbc7f0
K <k.rev.urtica@gmail.com>
2020-02-18 00:56:36 +0900

    🔥 Delete aos global settings

--- plugins/aos.js
+++ plugins/aos.js
@@ -1,21 +1,9 @@
-import Vue from "vue";
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 export default ({ app }) => {
   // eslint-disable-next-line new-cap
   app.AOS = new AOS.init({
-    // disable: 'phone'
     duration: 800
   });
 };
-
-Vue.mixin({
-  watch: {
-    "$route.path"() {
-      setTimeout(() => {
-        AOS.refresh();
-      }, 500);
-    }
-  }
-});
