1ca5db4ad4baf2a454215dc7c081a5e505195ef4
K <k.rev.urtica@gmail.com>
2021-04-16 20:59:13 +0900

    Remove nuxt/pwa

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,7 @@ export default {
   head: {
     htmlAttrs: {
       lang: "ja",
-      prefix: "og: http://ogp.me/ns#"
+      prefix: "og: http://ogp.me/ns#",
     },
     titleTemplate: "%s -- " + "Back-end Developer",
     title: "K",
@@ -21,48 +21,48 @@ export default {
       {
         hid: "description",
         name: "description",
-        content: description
+        content: description,
       },
       // OGP
       {
         hid: "og:site_name",
         property: "og:site_name",
-        content: siteTitle
+        content: siteTitle,
       },
       {
         hid: "og:type",
         property: "og:type",
-        content: "website"
+        content: "website",
       },
       {
         hid: "og:url",
         property: "og:url",
-        content: "https://k-urtica.github.io/"
+        content: "https://k-urtica.github.io/",
       },
       {
         hid: "og:title",
         property: "og:title",
-        content: siteTitle
+        content: siteTitle,
       },
       {
         hid: "og:description",
         property: "og:description",
-        content: description
+        content: description,
       },
       {
         hid: "og:image",
         property: "og:image",
-        content: "https://k-urtica.github.io/img/ogp-image.jpg"
+        content: "https://k-urtica.github.io/img/ogp-image.jpg",
       },
       // twitter
       {
         name: "twitter:card",
-        content: "summary_large_image"
+        content: "summary_large_image",
       },
       {
         name: "twitter:site",
-        content: "@k_urtica"
-      }
+        content: "@k_urtica",
+      },
     ],
     link: [
       { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
@@ -70,9 +70,9 @@ export default {
         rel: "stylesheet",
         type: "text/css",
         href:
-          "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css"
-      }
-    ]
+          "https://cdn.jsdelivr.net/npm/@mdi/font@4.x/css/materialdesignicons.min.css",
+      },
+    ],
   },
   loading: { color: "#fff" },
   css: [],
@@ -82,40 +82,21 @@ export default {
     [
       "@nuxtjs/google-analytics",
       {
-        id: process.env.G_ANALYTICS
-      }
-    ]
+        id: process.env.G_ANALYTICS,
+      },
+    ],
   ],
-  modules: ["@nuxtjs/dotenv", ["@nuxtjs/pwa", { icon: false }]],
+  modules: ["@nuxtjs/dotenv"],
   vuetify: {
     customVariables: ["~/assets/css/vuetify/variables.scss"],
     defaultAssets: {
-      icons: false
-    }
-  },
-  pwa: {
-    manifest: {
-      lang: "ja",
-      name: siteTitle,
-      short_name: siteTitle,
-      description,
-      background_color: "#69F0AE",
-      display: "browser" // ホームに追加ダイアログ非表示
+      icons: false,
     },
-    workbox: {
-      runtimeCaching: [
-        // googleフォント
-        {
-          urlPattern: "^https://fonts.(?:googleapis|gstatic).com/(.*)",
-          handler: "cacheFirst"
-        }
-      ]
-    }
   },
   build: {
-    extractCSS: true
+    extractCSS: true,
   },
   env: {
-    G_ANALYTICS
-  }
+    G_ANALYTICS,
+  },
 };
