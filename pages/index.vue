d4db5d1fa3812bac81299432121e50916c1fc2c1
K <k.rev.urtica@gmail.com>
2021-04-24 18:05:55 +0900

    🔥 Remove old code and assets

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,21 @@
 <template>
   <div>
     <header-hero />
-    <my-profile />
-    <my-skills />
-    <my-product />
+
+    <about class="py-12 mb-12" />
+    <skills class="py-12 mb-12" />
+    <portfolio class="py-12 mb-12" />
+    <links class="py-12 mb-12" />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HeaderHero from "~/components/main/HeaderHero.vue";
-import MyProfile from "~/components/main/MyProfile.vue";
-import MySkills from "~/components/main/MySkills.vue";
-import MyProduct from "~/components/main/MyProduct.vue";
+import HeaderHero from "~/components/HeaderHero.vue";
 
 export default Vue.extend({
   components: {
     HeaderHero,
-    MyProfile,
-    MySkills,
-    MyProduct,
   },
 });
 </script>
