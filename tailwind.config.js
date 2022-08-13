cd914fd01fb9160fe4d561ec3af30287c254666f
K <k.rev.urtica@gmail.com>
2022-08-13 11:20:23 +0900

    🔧 config: add content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,14 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    `components/**/*.{vue,js}`,
+    `layouts/**/*.vue`,
+    `pages/**/*.vue`,
+    `composables/**/*.{js,ts}`,
+    `plugins/**/*.{js,ts}`,
+    `App.{js,ts,vue}`,
+    `app.{js,ts,vue}`,
+  ],
   theme: {
     fontFamily: {
       sans: [
