eff17b7bf6d0c72e641d47305a27e270ae953b20
K <k.rev.urtica@gmail.com>
2023-12-02 09:30:23 +0900

    🔧 make eslint rules more stringent

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,27 @@ module.exports = {
     browser: true,
     node: true,
   },
-  extends: ['eslint:recommended', '@nuxt/eslint-config', 'prettier'],
-  plugins: [],
-  rules: {},
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    '@nuxt/eslint-config',
+    'prettier',
+  ],
+  rules: {
+    'no-console': 'off',
+    'import/order': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/require-default-prop': 'off',
+
+    'vue/define-macros-order': [
+      'error',
+      {
+        order: ['defineOptions', 'defineProps', 'defineEmits', 'defineSlots'],
+      },
+    ],
+    'vue/no-useless-v-bind': 'error',
+    'vue/no-unused-refs': 'error',
+    'vue/prefer-separate-static-class': 'error',
+  },
 };
