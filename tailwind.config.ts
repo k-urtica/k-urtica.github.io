07ac58fd8dc7457e62f0bcff0165f56448ef0d52
K <k.rev.urtica@gmail.com>
2024-07-11 09:28:23 +0900

    fix: add font-family

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,4 @@
 import type { Config } from 'tailwindcss';
 
 export default <Partial<Config>>{
-  theme: {
-    fontFamily: {
-      sans: [
-        'Exo',
-        'Helvetica Neue',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'Arial',
-        'メイリオ',
-        'Meiryo',
-        'sans-serif',
-      ],
-    },
-    extend: {
-      blur: {
-        xs: '2px',
-      },
-      width: {
-        112: '28rem',
-      },
-    },
-  },
 };
